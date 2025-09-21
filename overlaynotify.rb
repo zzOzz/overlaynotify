3199e62b3c3369785d90b9fe7084d1302eee6d5d
Vincent Lombard <vincent.lombard@gmail.com>
2025-09-21 10:24:01 +0200

    test package

--- overlaynotify.rb
+++ overlaynotify.rb
@@ -1,7 +1,7 @@
 class Overlaynotify < Formula
   desc "Lightweight macOS CLI tool for centered overlay notifications"
   homepage "https://github.com/zzOzz/overlaynotify"
-  url "https://github.com/zzOzz/overlaynotify/releases/download/v1.0.0b/OverlayNotify"
+  url "https://github.com/zzOzz/overlaynotify/releases/download/v1.0.0c/OverlayNotify"
   sha256 "b138790875dc74a65c70046863ba5ff76c91a57daa5918f171ce698cacfd1a12"
   license "MIT"
 
